ab6c51ddf0b91fea3588a301d27a960956cb0579
unknown <nadhemb@yahoo.com>
2022-05-04 16:20:29 +0100

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProduitsComponent } from './produits/produits.component';
+import { AddProduitComponent } from './add-produit/add-produit.component';
+import { FormsModule } from '@angular/forms';
+import { UpdateProduitComponent } from './update-produit/update-produit.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProduitsComponent,
+    AddProduitComponent,
+    UpdateProduitComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/produits/produits.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { Produit } from '../model/produit.model';
+import { ProduitService } from '../services/produit.service';
+
+@Component({
+  selector: 'app-produits',
+  templateUrl: './produits.component.html'
+})
+export class ProduitsComponent implements OnInit {
+
+    produits? : Produit[]; //un tableau de produits
+
+  constructor(private produitService: ProduitService) {
+   //this.produits=[];
+     }
+
+  ngOnInit(): void {
+
+    this.produits = this.produitService.listeProduits();
+  }
+
+  supprimerProduit(p: Produit)
+    {
+     // console.log(p);
+      let conf = confirm("Etes-vous sûr ?");
+      if (conf)
+        this.produitService.supprimerProduit(p);
+    } 
+
+}
